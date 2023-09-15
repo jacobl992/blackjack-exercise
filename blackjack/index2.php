89025f1543c8e41d95055f688192a6934a1c5298
Jake <jake_lewis12@hotmail.co.uk>
2023-09-15 16:10:07 +0100

    more changes

--- blackjack/index2.php
+++ blackjack/index2.php
@@ -3,7 +3,8 @@
 $numPlayers = 2;
 $cards = $numPlayers*2;
 $cardsArray = ['2 of clubs', '3 of clubs', '4 of clubs', '5 of clubs', '6 of clubs', '7 of clubs', '8 of clubs', '9 of clubs', '10 of clubs', 'Jack of clubs', 'Queen of clubs', 'King of clubs', 'Ace of clubs', '2 of diamonds', '3 of diamonds', '4 of diamonds', '5 of diamonds', '6 of diamonds', '7 of diamonds', '8 of diamonds', '9 of diamonds', '10 of diamonds', 'Jack of diamonds', 'Queen of diamonds', 'King of diamonds', 'Ace of diamonds', '2 of hearts', '3 of hearts', '4 of hearts', '5 of hearts', '6 of hearts', '7 of hearts', '8 of hearts', '9 of hearts', '10 of hearts', 'Jack of hearts', 'Queen of hearts', 'King of hearts', 'Ace of hearts', '2 of spades', '3 of spades', '4 of spades', '5 of spades', '6 of spades', '7 of spades', '8 of spades', '9 of spades', '10 of spades', 'Jack of spades', 'Queen of spades', 'King of spades', 'Ace of spades'];
-
+$playerCards = [];
+$playerScores = [];
 
 $i = 1;
 while ($i <= $cards) {
@@ -17,12 +18,24 @@ while ($i <= $cards) {
     } else {
         $cardValue = 10;
     }
-    $player = "Dynamic player name";
-     echo '<h3>' . $player . '</h3>';
     echo '<p>' . $pickedCard . '</p>';
     echo '<p>' . 'Value of card is ' . $cardValue . '</p>';
     echo '<p>' . '   ------------------' . '</p>';
     //add $pickedCard to an array to some for players later
+    array_push($playerCards, $pickedCard);
+    array_push($playerScores, $cardValue);
     unset ($cardsArray[$randomkey]);
     $i++;
 }
+
+echo '<pre>';
+print_r($playerCards);
+echo '</pre>';
+
+echo '<pre>';
+print_r($playerScores);
+echo '</pre>';
+
+//let's say minimum players is 2
+//maximum is five
+//if there are two players 
